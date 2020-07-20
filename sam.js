d3f515355952cfc0d97797a5cb1105d92ad8a5ab
Sam Li <55217028+wsli1998@users.noreply.github.com>
2020-07-20 22:01:31 +0800

    add house

--- sam.js
+++ sam.js
@@ -1,6 +1,102 @@
-var msg = 'Hello World';
+var msg = "Hello World";
 console.log(msg);
 
+let canvas = document.querySelector("#mycanvas");
+let ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 500;
+
+function drawMyHouse() {
+  ctx.fillStyle = "#f6e204";
+  ctx.fillRect(50, 350, 200, 200);
+  ctx.beginPath();
+  ctx.moveTo(50, 350);
+  ctx.lineTo(150, 250);
+  ctx.lineTo(250, 350);
+  ctx.closePath();
+  ctx.fillStyle = "#6a1a10";
+  ctx.fill();
+
+  ctx.strokeStyle = "#0173ab";
+  ctx.strokeRect(75, 375, 50, 50);
+  ctx.beginPath();
+  ctx.moveTo(75, 400);
+  ctx.lineTo(125, 400);
+  ctx.moveTo(100, 375);
+  ctx.lineTo(100, 425);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.strokeStyle = "#6a1a10";
+  ctx.strokeRect(175, 450, 25, 50);
+  ctx.strokeRect(200, 450, 25, 50);
+
+  ctx.fillStyle = "#ba2736";
+  ctx.beginPath();
+  ctx.arc(190, 470, 5, 0, Math.PI * 2);
+  ctx.arc(210, 470, 5, 0, Math.PI * 2);
+  ctx.fill();
+
+  ctx.fillStyle = "#6b4224";
+  ctx.fillRect(400, 300, 200, 200);
+
+  ctx.beginPath();
+  ctx.arc(320, 450, 80, 0, 0.4 * Math.PI);
+  ctx.lineTo(400, 500);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.fillStyle = "#335617";
+  ctx.beginPath();
+  ctx.arc(450, 200, 190, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+
+  //果實
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(450, 100, 15, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(350, 200, 15, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(430, 280, 15, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+
+  //雲
+  ctx.fillStyle = "#fff";
+  ctx.beginPath();
+  ctx.arc(80, 100, 30, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(120, 75, 35, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(160, 75, 30, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(200, 100, 30, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(120, 125, 30, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(160, 125, 35, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+}
+// drawMyHouse();
+
+// https://medium.com/unalai/回到幼幼班-用-canvas-畫-我的家-c7fcfd1d16a
 
 // to run : node sam.js
 
